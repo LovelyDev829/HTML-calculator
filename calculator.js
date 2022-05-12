c5c50843bed84d12ef631dec8666a210d2e8fe37
LovelyDev829 <lovelydeveloper829@gmail.com>
2022-05-12 08:21:34 +0300

    Simple html calculator.

---
+++ calculator.js
@@ -0,0 +1,498 @@
+function mouseover_CE(){
+    document.getElementById("CE").style.backgroundColor="gray";
+    // document.this.style.backgroundColor="white";
+}
+function mouseover_C(){
+    document.getElementById("C").style.backgroundColor="gray";
+    // document.this.style.backgroundColor="white";
+}
+function mouseover_DEL(){
+    document.getElementById("DEL").style.backgroundColor="gray";
+    // document.this.style.backgroundColor="white";
+}
+function mouseover_divi(){
+    document.getElementById("divi").style.backgroundColor="gray";
+    // document.this.style.backgroundColor="white";
+}
+function mouseover_multi(){
+    document.getElementById("multi").style.backgroundColor="gray";
+    // document.this.style.backgroundColor="white";
+}
+function mouseover_minus(){
+    document.getElementById("minus").style.backgroundColor="gray";
+    // document.this.style.backgroundColor="white";
+}
+function mouseover_plus(){
+    document.getElementById("plus").style.backgroundColor="gray";
+    // document.this.style.backgroundColor="white";
+}
+function mouseover_is(){
+    document.getElementById("is").style.backgroundColor="gray";
+    // document.this.style.backgroundColor="white";
+}
+function mouseover_dot(){
+    document.getElementById("dot").style.backgroundColor="gray";
+    // document.this.style.backgroundColor="white";
+}
+function mouseover_symbol(){
+    document.getElementById("symbol").style.backgroundColor="gray";
+    // document.this.style.backgroundColor="white";
+}
+function mouseover_one(){
+    document.getElementById("one").style.backgroundColor="gray";
+}
+function mouseover_two(){
+    document.getElementById("two").style.backgroundColor="gray";
+    // document.this.style.backgroundColor="white";
+}
+function mouseover_three(){
+    document.getElementById("three").style.backgroundColor="gray";
+    // document.this.style.backgroundColor="white";
+}
+function mouseover_four(){
+    document.getElementById("four").style.backgroundColor="gray";
+    // document.this.style.backgroundColor="white";
+}
+function mouseover_five(){
+    document.getElementById("five").style.backgroundColor="gray";
+    // document.this.style.backgroundColor="white";
+}
+function mouseover_six(){
+    document.getElementById("six").style.backgroundColor="gray";
+    // document.this.style.backgroundColor="white";
+}
+function mouseover_seven(){
+    document.getElementById("seven").style.backgroundColor="gray";
+    // document.this.style.backgroundColor="white";
+}
+function mouseover_eight(){
+    document.getElementById("eight").style.backgroundColor="gray";
+    // document.this.style.backgroundColor="white";
+}
+function mouseover_nine(){
+    document.getElementById("nine").style.backgroundColor="gray";
+    // document.this.style.backgroundColor="white";
+}
+function mouseover_zero(){
+    document.getElementById("zero").style.backgroundColor="gray";
+    // document.this.style.backgroundColor="white";
+}
+
+
+
+
+
+
+function mouseleave_CE(){
+    document.getElementById("CE").style.backgroundColor="rgb(30,30,30)";
+    document.getElementById("CE").style.color="white";
+}
+function mouseleave_C(){
+    document.getElementById("C").style.backgroundColor="rgb(30,30,30)";
+    document.getElementById("C").style.color="white";
+}
+function mouseleave_DEL(){
+    document.getElementById("DEL").style.backgroundColor="rgb(30,30,30)";
+    document.getElementById("DEL").style.color="white";
+}
+function mouseleave_divi(){
+    document.getElementById("divi").style.backgroundColor="rgb(30,30,30)";
+    document.getElementById("divi").style.color="white";
+}
+function mouseleave_multi(){
+    document.getElementById("multi").style.backgroundColor="rgb(30,30,30)";
+    document.getElementById("multi").style.color="white";
+}
+function mouseleave_minus(){
+    document.getElementById("minus").style.backgroundColor="rgb(30,30,30)";
+    document.getElementById("minus").style.color="white";
+}
+function mouseleave_plus(){
+    document.getElementById("plus").style.backgroundColor="rgb(30,30,30)";
+    document.getElementById("plus").style.color="white";
+}
+function mouseleave_is(){
+    document.getElementById("is").style.backgroundColor="rgb(30,30,30)";
+    document.getElementById("is").style.color="white";
+}
+function mouseleave_dot(){
+    document.getElementById("dot").style.backgroundColor="rgb(30,30,30)";
+    document.getElementById("dot").style.color="white";
+}
+function mouseleave_symbol(){
+    document.getElementById("symbol").style.backgroundColor="rgb(30,30,30)";
+    document.getElementById("symbol").style.color="white";
+}
+function mouseleave_one(){
+    document.getElementById("one").style.backgroundColor="rgb(30,30,30)";
+    document.getElementById("one").style.color="white";
+}
+function mouseleave_two(){
+    document.getElementById("two").style.backgroundColor="rgb(30,30,30)";
+    document.getElementById("two").style.color="white";
+}
+function mouseleave_three(){
+    document.getElementById("three").style.backgroundColor="rgb(30,30,30)";
+    document.getElementById("three").style.color="white";
+}
+function mouseleave_four(){
+    document.getElementById("four").style.backgroundColor="rgb(30,30,30)";
+    document.getElementById("four").style.color="white";
+}
+function mouseleave_five(){
+    document.getElementById("five").style.backgroundColor="rgb(30,30,30)";
+    document.getElementById("five").style.color="white";
+}
+function mouseleave_six(){
+    document.getElementById("six").style.backgroundColor="rgb(30,30,30)";
+    document.getElementById("six").style.color="white";
+}
+function mouseleave_seven(){
+    document.getElementById("seven").style.backgroundColor="rgb(30,30,30)";
+    document.getElementById("seven").style.color="white";
+}
+function mouseleave_eight(){
+    document.getElementById("eight").style.backgroundColor="rgb(30,30,30)";
+    document.getElementById("eight").style.color="white";
+}
+function mouseleave_nine(){
+    document.getElementById("nine").style.backgroundColor="rgb(30,30,30)";
+    document.getElementById("nine").style.color="white";
+}
+function mouseleave_zero(){
+    document.getElementById("zero").style.backgroundColor="rgb(30,30,30)";
+    document.getElementById("sero").style.color="white";
+}
+
+
+
+
+
+
+function down_CE(){
+    document.getElementById("CE").style.backgroundColor="rgb(200,200,200)";
+    document.getElementById("CE").style.color="black";
+}
+function down_C(){
+    document.getElementById("C").style.backgroundColor="rgb(200,200,200)";
+    document.getElementById("C").style.color="black";
+}
+function down_DEL(){
+    document.getElementById("DEL").style.backgroundColor="rgb(200,200,200)";
+    document.getElementById("DEL").style.color="black";
+}
+function down_divi(){
+    document.getElementById("divi").style.backgroundColor="rgb(200,200,200)";
+    document.getElementById("divi").style.color="black";
+}
+function down_multi(){
+    document.getElementById("multi").style.backgroundColor="rgb(200,200,200)";
+    document.getElementById("multi").style.color="black";
+}
+function down_minus(){
+    document.getElementById("minus").style.backgroundColor="rgb(200,200,200)";
+    document.getElementById("minus").style.color="black";
+}
+function down_plus(){
+    document.getElementById("plus").style.backgroundColor="rgb(200,200,200)";
+    document.getElementById("plus").style.color="black";
+}
+function down_is(){
+    document.getElementById("is").style.backgroundColor="rgb(200,200,200)";
+    document.getElementById("is").style.color="black";
+}
+function down_symbol(){
+    document.getElementById("symbol").style.backgroundColor="rgb(200,200,200)";
+    document.getElementById("symbol").style.color="black";
+}
+function down_dot(){
+    document.getElementById("dot").style.backgroundColor="rgb(200,200,200)";
+    document.getElementById("dot").style.color="black";
+}
+function down_one(){
+    document.getElementById("one").style.backgroundColor="rgb(200,200,200)";
+    document.getElementById("one").style.color="black";
+}
+function down_two(){
+    document.getElementById("two").style.backgroundColor="rgb(200,200,200)";
+    document.getElementById("two").style.color="black";
+}
+function down_three(){
+    document.getElementById("three").style.backgroundColor="rgb(200,200,200)";
+    document.getElementById("three").style.color="black";
+}
+function down_four(){
+    document.getElementById("four").style.backgroundColor="rgb(200,200,200)";
+    document.getElementById("four").style.color="black";
+}
+function down_five(){
+    document.getElementById("five").style.backgroundColor="rgb(200,200,200)";
+    document.getElementById("five").style.color="black";
+}
+function down_six(){
+    document.getElementById("six").style.backgroundColor="rgb(200,200,200)";
+    document.getElementById("six").style.color="black";
+}
+function down_seven(){
+    document.getElementById("seven").style.backgroundColor="rgb(200,200,200)";
+    document.getElementById("seven").style.color="black";
+}
+function down_eight(){
+    document.getElementById("eight").style.backgroundColor="rgb(200,200,200)";
+    document.getElementById("eight").style.color="black";
+}
+function down_nine(){
+    document.getElementById("nine").style.backgroundColor="rgb(200,200,200)";
+    document.getElementById("nine").style.color="black";
+}
+function down_zero(){
+    document.getElementById("zero").style.backgroundColor="rgb(200,200,200)";
+    document.getElementById("zero").style.color="black";
+}
+
+
+
+
+let chk="0";
+var res="0";
+var p_btn="no";
+var val="0";
+var chk_del="0";
+var chk_dot_hou="0";
+
+function calc(){
+    if(p_btn=="no"){
+        res=document.getElementById("screen_id").value;
+    }
+    else{
+        var ttt=document.getElementById("screen_id").value;
+        var a=Number(res);
+        var b=Number(ttt);
+        if(p_btn=="plus")          var c=a+b;
+        else if(p_btn=="minus")    var c=a-b;
+        else if(p_btn=="multiple") var c=a*b;
+        else if(p_btn=="divide")   var c=a/b;
+        res=c; 
+        p_btn="no";
+        document.getElementById("screen_id").value=res;
+    }
+    chk_del="1";
+}
+
+function up_CE(){
+    document.getElementById("CE").style.backgroundColor="grey";
+    document.getElementById("CE").style.color="white";
+    document.getElementById("screen_id").value ='0';
+    chk="0";
+    res="0";
+    p_btn="no";
+    val="0";
+    chk_del="0";
+    chk_dot_hou="0";
+}
+function up_C(){
+    document.getElementById("C").style.backgroundColor="grey";
+    document.getElementById("C").style.color="white";
+    document.getElementById("screen_id").value ='0';
+    chk="0";
+    res="0";
+    p_btn="no";
+    val="0";
+    chk_del="0";
+    chk_dot_hou="0";
+}
+function up_DEL(){
+    document.getElementById("DEL").style.backgroundColor="grey";
+    document.getElementById("DEL").style.color="white";
+    var ttt=document.getElementById("screen_id").value;
+    var tnt=ttt.slice(0,-1);
+    document.getElementById("screen_id").value=tnt;
+}
+function up_divi(){
+    document.getElementById("divi").style.backgroundColor="grey";
+    document.getElementById("divi").style.color="white";
+    calc();
+    p_btn="divide";
+}
+function up_multi(){
+    document.getElementById("multi").style.backgroundColor="grey";
+    document.getElementById("multi").style.color="white";
+    calc();
+    p_btn="multiple";
+}
+function up_minus(){
+    document.getElementById("minus").style.backgroundColor="grey";
+    document.getElementById("minus").style.color="white";
+    calc();
+    p_btn="minus";
+}
+function up_plus(){
+    document.getElementById("plus").style.backgroundColor="grey";
+    document.getElementById("plus").style.color="white";
+    calc();
+    p_btn="plus";
+}
+function up_is(){
+    document.getElementById("is").style.backgroundColor="grey";
+    document.getElementById("is").style.color="white";
+    calc();
+}
+
+
+
+
+function up_symbol(){
+    document.getElementById("symbol").style.backgroundColor="grey";
+    document.getElementById("symbol").style.color="white";
+    var ttt=document.getElementById("screen_id").value;
+    var tnt=Number(ttt)*(-1);
+    document.getElementById("screen_id").value=tnt;
+}
+function up_one(){
+    document.getElementById("one").style.backgroundColor="grey";
+    document.getElementById("one").style.color="white";
+
+    var value_=document.getElementById("screen_id").value;
+    if(chk_del=="1"){
+        chk_del="0";
+        value_="0";
+    }
+    if(Number(value_)>'0') value_+='1';
+    else if(chk_dot_hou=="1") value_='0.1';
+    else value_='1';
+    document.getElementById("screen_id").value = value_;
+
+}
+function up_two(){
+    document.getElementById("two").style.backgroundColor="grey";
+    document.getElementById("two").style.color="white";
+    var value_=document.getElementById("screen_id").value;
+    if(chk_del=="1"){
+        chk_del="0";
+        value_="0";
+    }
+    if(Number(value_)>'0') value_+='2';
+    else if(chk_dot_hou=="1") value_='0.2';
+    else value_='2';
+    document.getElementById("screen_id").value = value_;
+}
+function up_three(){
+    document.getElementById("three").style.backgroundColor="grey";
+    document.getElementById("three").style.color="white";
+    var value_=document.getElementById("screen_id").value;
+    if(chk_del=="1"){
+        chk_del="0";
+        value_="0";
+    }
+    if(Number(value_)>'0') value_+='3';
+    else if(chk_dot_hou=="1") value_='0.3';
+    else value_='3';
+    document.getElementById("screen_id").value = value_;
+}
+function up_four(){
+    document.getElementById("four").style.backgroundColor="grey";
+    document.getElementById("four").style.color="white";
+    var value_=document.getElementById("screen_id").value;
+    if(chk_del=="1"){
+        chk_del="0";
+        value_="0";
+    }
+    if(Number(value_)>'0') value_+='4';
+    else if(chk_dot_hou=="1") value_='0.4';
+    else value_='4';
+    document.getElementById("screen_id").value = value_;
+}
+function up_five(){
+    document.getElementById("five").style.backgroundColor="grey";
+    document.getElementById("five").style.color="white";
+    var value_=document.getElementById("screen_id").value;
+    if(chk_del=="1"){
+        chk_del="0";
+        value_="0";
+    }
+    if(Number(value_)>'0') value_+='5';
+    else if(chk_dot_hou=="1") value_='0.5';
+    else value_='5';
+    document.getElementById("screen_id").value = value_;
+}
+function up_six(){
+    document.getElementById("six").style.backgroundColor="grey";
+    document.getElementById("six").style.color="white";
+    var value_=document.getElementById("screen_id").value;
+    if(chk_del=="1"){
+        chk_del="0";
+        value_="0";
+    }
+    if(Number(value_)>'0') value_+='6';
+    else if(chk_dot_hou=="1") value_='0.6';
+    else value_='6';
+    document.getElementById("screen_id").value = value_;
+}
+function up_seven(){
+    document.getElementById("seven").style.backgroundColor="grey";
+    document.getElementById("seven").style.color="white";
+    var value_=document.getElementById("screen_id").value;
+    if(chk_del=="1"){
+        chk_del="0";
+        value_="0";
+    }
+    if(Number(value_)>'0') value_+='7';
+    else if(chk_dot_hou=="1") value_='0.7';
+    else value_='7';
+    document.getElementById("screen_id").value = value_;
+}
+function up_eight(){
+    document.getElementById("eight").style.backgroundColor="grey";
+    document.getElementById("eight").style.color="white";
+    var value_=document.getElementById("screen_id").value;
+    if(chk_del=="1"){
+        chk_del="0";
+        value_="0";
+    }
+    if(Number(value_)>'0') value_+='8';
+    else if(chk_dot_hou=="1") value_='0.8';
+    else value_='8';
+    document.getElementById("screen_id").value = value_;
+}
+function up_nine(){
+    document.getElementById("nine").style.backgroundColor="grey";
+    document.getElementById("nine").style.color="white";
+    var value_=document.getElementById("screen_id").value;
+    if(chk_del=="1"){
+        chk_del="0";
+        value_="0";
+    }
+    if(Number(value_)>'0') value_+='9';
+    else if(chk_dot_hou=="1") value_='0.9';
+    else value_='9';
+    document.getElementById("screen_id").value = value_;
+}
+function up_zero(){
+    document.getElementById("zero").style.backgroundColor="grey";
+    document.getElementById("zero").style.color="white";
+    var value_=document.getElementById("screen_id").value;
+    if(chk_del=="1"){
+        chk_del="0";
+        value_="0";
+    }
+    if(Number(value_)>'0') value_+='0';
+    else value_='0';
+    document.getElementById("screen_id").value = value_;
+}
+function up_dot(){
+    document.getElementById("dot").style.backgroundColor="grey";
+    document.getElementById("dot").style.color="white";
+    if(chk=="1") return;
+    chk="1";
+    var value_=document.getElementById("screen_id").value;
+    if(chk_del=="1"){
+        chk_del="0";
+        value_="0";
+    }
+    if(Number(value_)>'0') value_+='.';
+    else if(chk=="1") value_='0.';
+    else chk_dot_hou="1";
+    chk="1";
+    document.getElementById("screen_id").value = value_;
+}
+
